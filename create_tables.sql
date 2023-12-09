1e86ef416b137e2951be689db16bccfe8eacfe1f
Ben Puryear <54869170+Ben10164@users.noreply.github.com>
2023-12-09 12:30:48 -0800

    Made tables

--- create_tables.sql
+++ create_tables.sql
@@ -1,67 +1,133 @@
-DROP TABLE IF EXISTS Result;
-DROP TABLE IF EXISTS DriverGroupCar;
-DROP TABLE IF EXISTS Car;
-DROP TABLE IF EXISTS DriverGroup;
-DROP TABLE IF EXISTS Driver;
+drop table if exists DriverResult;
+
+DROP TABLE IF EXISTS CarTyre;
+
+DROP TABLE IF EXISTS Tyre;
+
 DROP TABLE IF EXISTS Team;
-DROP TABLE IF EXISTS Class;
--- This will be the file that creates the tables
 
-CREATE TABLE Class (
-    class_id INT AUTO_INCREMENT PRIMARY KEY,
-    class_name VARCHAR(50)
+drop table if exists Result;
+
+DROP TABLE IF EXISTS CarNumber;
+
+DROP TABLE IF EXISTS Car;
+
+DROP TABLE IF EXISTS CarClass;
+
+DROP TABLE IF EXISTS Race;
+
+DROP TABLE IF EXISTS Driver;
+
+-- Driver table
+CREATE TABLE IF NOT EXISTS Driver (
+    id INT NOT NULL AUTO_INCREMENT,
+    full_name VARCHAR(50),
+    country CHAR(4),
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE Team (
-    team_id INT AUTO_INCREMENT PRIMARY KEY,
-    team_name VARCHAR(50),
-    team_country VARCHAR(50)
+-- Race table
+CREATE TABLE IF NOT EXISTS Race (
+    id INT NOT NULL AUTO_INCREMENT,
+    race_yr INT NOT NULL,
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE Driver (
-    driver_id INT AUTO_INCREMENT PRIMARY KEY,
-    driver_country VARCHAR(3),
-    driver_name VARCHAR(50)
+-- CarClass table
+CREATE TABLE IF NOT EXISTS CarClass(
+    id VARCHAR(50) NOT NULL,
+    class_desc VARCHAR(50),
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE DriverGroup (
-    driver_group_id INT AUTO_INCREMENT,
-    team_id INT,
-    driver_id INT,
-    PRIMARY KEY(driver_group_id, driver_id),
-    FOREIGN KEY (team_id) REFERENCES Team(team_id),
-    FOREIGN KEY (driver_id) REFERENCES Driver(driver_id)
-);
-
-CREATE TABLE Car (
-    car_num INT,
-    race_year INT,
-    team_id INT,
-    class_id INT,
+-- Car table
+CREATE TABLE IF NOT EXISTS Car (
+    id INT NOT NULL AUTO_INCREMENT,
+    car_class_id VARCHAR(50),
     car_chassis VARCHAR(50),
     car_engine VARCHAR(50),
-    PRIMARY KEY (car_num, race_year),
-    FOREIGN KEY (team_id) REFERENCES Team(team_id),
-    FOREIGN KEY (class_id) REFERENCES Class(class_id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (car_class_id) REFERENCES CarClass(id)
 );
 
-CREATE TABLE DriverGroupCar (
-    driver_group_id INT AUTO_INCREMENT,
-    car_num INT,
-    race_year INT,
-    PRIMARY KEY (driver_group_id, race_year),
-    FOREIGN KEY (driver_group_id) REFERENCES DriverGroup(driver_group_id),
-    FOREIGN KEY (car_num, race_year) REFERENCES Car(car_num, race_year)
+-- CarNumber table
+CREATE TABLE IF NOT EXISTS CarNumber (
+    id INT NOT NULL AUTO_INCREMENT,
+    race_id INT NOT NULL,
+    car_id INT NOT NULL,
+    car_number VARCHAR(3) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (race_id) REFERENCES Race(id),
+    FOREIGN KEY (car_id) REFERENCES Car(id)
 );
 
-CREATE TABLE Result (
-    car_num INT,
-    race_year INT,
-    car_result INT,
-    car_time FLOAT,
-    car_laps INT,
-    car_distance FLOAT,
-    car_retirement_reason VARCHAR(50),
-    PRIMARY KEY (car_num, race_year),
-    FOREIGN KEY (car_num, race_year) REFERENCES DriverGroupCar(car_num, race_year)
+CREATE TABLE IF NOT EXISTS Result (
+    id INT NOT NULL AUTO_INCREMENT,
+    race_id INT NOT NULL,
+    car_id INT NOT NULL,
+    pos VARCHAR(3) NOT NULL,
+    laps INT,
+    -- format used by the data [DECIMAL(8, 3)]
+    distance DECIMAL(8, 3) NULL, 
+    racing_time TIME,
+    retirement_reason VARCHAR(128) NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (race_id) REFERENCES Race(id),
+    FOREIGN KEY (car_id) REFERENCES CarNumber(id)
 );
+
+-- Team table
+CREATE TABLE IF NOT EXISTS Team (
+    id INT NOT NULL AUTO_INCREMENT,
+    team_name VARCHAR(64),
+    country CHAR(4),
+    PRIMARY KEY (id)
+);
+
+-- Tyre table
+CREATE TABLE IF NOT EXISTS Tyre (
+    id CHAR(2) NOT NULL,
+    tyre_name VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+-- CarTyre table
+CREATE TABLE IF NOT EXISTS CarTyre (
+    car_id INT NOT NULL,
+    tyre_id CHAR(2) NOT NULL,
+    PRIMARY KEY (car_id, tyre_id),
+    FOREIGN KEY (car_id) REFERENCES CarNumber(id),
+    FOREIGN KEY (tyre_id) REFERENCES Tyre(id)
+);
+
+-- DriverResult table
+CREATE TABLE IF NOT EXISTS DriverResult(
+    driver_id INT NOT NULL,
+    result_id INT NOT NULL,
+    place INT NOT NULL,
+    PRIMARY KEY (driver_id, result_id),
+    FOREIGN KEY (driver_id) REFERENCES Driver(id),
+    FOREIGN KEY (result_id) REFERENCES Result(id)
+);
+
+-- Insert data into Tyre
+INSERT INTO
+    Tyre (id, tyre_name)
+VALUES
+    ('A', 'Avon'),
+    ('BF', 'BF Goodrich'),
+    ('Br', 'Barum'),
+    ('B', 'Bridgestone'),
+    ('C', 'Continental'),
+    ('D', 'Dunlop'),
+    ('E', 'Englebert'),
+    ('F', 'Firestone'),
+    ('G', 'Goodyear'),
+    ('H', 'Hankook'),
+    ('I', 'India'),
+    ('Kl', 'Klèber'),
+    ('Ku', 'Kumho'),
+    ('M', 'Michelin'),
+    ('P', 'Pirelli'),
+    ('R', 'Rapson'),
+    ('Y', 'Yokohama');
