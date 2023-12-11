b8f63fe52727f02146f9af0a77961aeab5a4b470
Ben Puryear <54869170+Ben10164@users.noreply.github.com>
2023-12-11 00:19:38 -0800

    Finished db

--- create_tables.sql
+++ create_tables.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS TeamResult;
 drop table if exists DriverResult;
 DROP TABLE IF EXISTS CarTyre;
 DROP TABLE IF EXISTS Tyre;
@@ -36,6 +37,11 @@ CREATE TABLE IF NOT EXISTS CarClass(
     PRIMARY KEY (id)
 );
 
+-- Populate the CarClass table (ill go back and add descriptions to these later)
+INSERT INTO CarClass(id)
+SELECT DISTINCT car_class
+FROM results_in;
+
 -- Car table
 CREATE TABLE IF NOT EXISTS Car (
     id INT NOT NULL AUTO_INCREMENT,
@@ -58,16 +64,14 @@ CREATE TABLE IF NOT EXISTS CarNumber (
 
 -- Result table
 CREATE TABLE IF NOT EXISTS Result (
-    -- id INT NOT NULL AUTO_INCREMENT,
     race_id INT NOT NULL,
     car_number VARCHAR(3),
     pos VARCHAR(3) NOT NULL,
     laps INT,
     -- format used by the data [DECIMAL(8, 3)]
-    distance DECIMAL(8, 3) NULL, 
+    distance DECIMAL(8, 3) NULL,
     racing_time TIME,
-    retirement_reason VARCHAR(128) NULL,
-    -- PRIMARY KEY (id),
+    retirement_reason VARCHAR(50) NULL,
     PRIMARY KEY (race_id, car_number),
     FOREIGN KEY (race_id, car_number) REFERENCES CarNumber(race_id, car_number)
 );
@@ -75,14 +79,15 @@ CREATE TABLE IF NOT EXISTS Result (
 -- Team table
 CREATE TABLE IF NOT EXISTS Team (
     id INT NOT NULL AUTO_INCREMENT,
-    team_name VARCHAR(64),
+    team_name VARCHAR(100),
     country CHAR(4),
     PRIMARY KEY (id)
 );
 
-/*markdown
-# Tyre
-*/
+-- Populate the CarClass table (ill go back and add descriptions to these later)
+INSERT INTO Team(team_name)
+SELECT DISTINCT team_name
+FROM results_in;
 
 -- Tyre table
 CREATE TABLE IF NOT EXISTS Tyre (
@@ -92,10 +97,8 @@ CREATE TABLE IF NOT EXISTS Tyre (
 );
 
 -- Insert data into Tyre
-INSERT INTO
-    Tyre (id, tyre_name)
-VALUES
-    ('A', 'Avon'),
+INSERT INTO Tyre (id, tyre_name)
+VALUES ('A', 'Avon'),
     ('BF', 'BF Goodrich'),
     ('Br', 'Barum'),
     ('B', 'Bridgestone'),
@@ -129,17 +132,25 @@ CREATE TABLE IF NOT EXISTS DriverResult(
     race_id INT NOT NULL,
     car_number VARCHAR(3),
     driver_order INT,
+    team_name VARCHAR(100),
     PRIMARY KEY (driver_id, race_id, car_number),
     FOREIGN KEY (driver_id) REFERENCES Driver(id),
     FOREIGN KEY (race_id, car_number) REFERENCES Result(race_id, car_number)
 );
 
+CREATE TABLE IF NOT EXISTS TeamResult(
+    team_id INT,
+    race_id INT NOT NULL, 
+    car_number VARCHAR(3),
+    ord_num INT,
+    PRIMARY KEY(team_id, race_id, car_number),
+    FOREIGN KEY (team_id) REFERENCES Team(id),
+    FOREIGN KEY (race_id, car_number) REFERENCES Result(race_id, car_number)
+);
+
 -- Drop the procedure if it already exists
 DROP PROCEDURE IF EXISTS process_drivers;
-
-DELIMITER //
-
--- Create the procedure
+DELIMITER // -- Create the procedure
 CREATE PROCEDURE process_drivers()
 BEGIN
     -- Declare variables
@@ -159,12 +170,8 @@ BEGIN
     DECLARE res_distance DECIMAL(8, 3);
     DECLARE res_racing_time TIME(3);
     DECLARE res_reason VARCHAR(128);
-
     DECLARE new_driver_id INT;
     DECLARE new_drv_name VARCHAR(64);
-    DECLARE new_drv_title VARCHAR(16);
-    DECLARE new_drv_fname VARCHAR(32);
-    DECLARE new_drv_lname VARCHAR(32);
     DECLARE new_drv_nick VARCHAR(32);
     DECLARE new_drv_nm_sfx VARCHAR(4);
     DECLARE new_drv_sex CHAR(1);
@@ -177,158 +184,258 @@ BEGIN
     DECLARE new_carnb_id INTEGER;
     DECLARE new_team_id INTEGER;
     DECLARE new_res_id INT; -- Added declaration for new_res_id
-
-    -- Declare the cursor outside of the BEGIN...END block
     DECLARE done INT DEFAULT FALSE;
 
-    -- Declare CONTINUE HANDLER inside the BEGIN...END block
+      /* Team data */
+  DECLARE new_team_name  VARCHAR(64);
+  DECLARE new_team_cntry CHAR(4);
+  DECLARE new_team_priv  TINYINT;
+  DECLARE new_team_ord   INT;
+
+    -- Declare the cursor outside of the BEGIN...END block
     DECLARE cur_results CURSOR FOR
-        SELECT id, race_yr, pos, car_class, car_nbr, team_cntry, team_name, drivers_cntry, drivers_name, car_chassis, car_engine, car_tyres, laps, distance, racing_time, reason
-        FROM results_in
-        WHERE processed IS FALSE;
+    SELECT id,
+        race_yr,
+        pos,
+        car_class,
+        car_nbr,
+        team_cntry,
+        team_name,
+        drivers_cntry,
+        drivers_name,
+        car_chassis,
+        car_engine,
+        car_tyres,
+        laps,
+        distance,
+        racing_time,
+        reason
+    FROM results_in
+    WHERE processed IS FALSE;
 
-    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+    -- Declare CONTINUE HANDLER inside the BEGIN...END block
+    DECLARE CONTINUE HANDLER FOR NOT FOUND
+    SET done = TRUE;
 
-     -- Open the cursor
-        OPEN cur_results;
+    -- Open the cursor
+    OPEN cur_results;
 
     -- Start the procedure
-    BEGIN
-        res_loop: LOOP
-            SET new_car_id = NULL;
-            SET new_carnb_id = NULL;
-            SET new_team_id = NULL;
-            SET new_driver_id = NULL;
+    BEGIN res_loop: LOOP
+        SET new_car_id = NULL;
+        SET new_carnb_id = NULL;
+        SET new_team_id = NULL;
+        SET new_driver_id = NULL;
 
-            FETCH cur_results INTO
-                res_id, res_race_yr, res_pos, res_car_class, res_car_nbr, res_team_cntry, res_team_name, res_drivers_cntry, res_drivers_name,
-                res_car_chassis, res_car_engine, res_car_tyres, res_laps, res_distance, res_racing_time, res_reason;
+        FETCH cur_results INTO res_id,
+            res_race_yr,
+            res_pos,
+            res_car_class,
+            res_car_nbr,
+            res_team_cntry,
+            res_team_name,
+            res_drivers_cntry,
+            res_drivers_name,
+            res_car_chassis,
+            res_car_engine,
+            res_car_tyres,
+            res_laps,
+            res_distance,
+            res_racing_time,
+            res_reason;
 
-            IF done THEN
-                LEAVE res_loop;
+        IF done THEN
+            LEAVE res_loop;
+        END IF;
+
+        SET new_drv_ord = 0;
+
+        -- Start a transaction
+        START TRANSACTION;
+
+        WHILE res_drivers_name IS NOT NULL DO
+            SET new_drv_name = SUBSTRING_INDEX(res_drivers_name, '|', 1);
+            SET new_drv_cntry = SUBSTRING_INDEX(res_drivers_cntry, '|', 1);
+            SET new_drv_ord = new_drv_ord + 1;
+
+            IF new_drv_name != '' AND new_drv_cntry != '' THEN
+                SET tmp_drv_name = new_drv_name;
+
+                IF new_drv_name LIKE '%''%''%' THEN -- name with two single quotes
+                    SET fst_qt_idx = INSTR(new_drv_name, '''');
+                    SET snd_qt_idx = LOCATE('''', new_drv_name, fst_qt_idx + 1);
+                    SET tmp_drv_name = SUBSTRING(new_drv_name, 1, fst_qt_idx - 1);
+                    SET tmp_drv_name = CONCAT(tmp_drv_name, TRIM(SUBSTRING(new_drv_name, snd_qt_idx + 1)));
+                END IF;
+
+                -- clean braces in name and trim
+                SET tmp_drv_name = REPLACE(tmp_drv_name, '(', '');
+                SET tmp_drv_name = REPLACE(tmp_drv_name, ')', '');
+                SET new_drv_name = TRIM(tmp_drv_name);
+
+                BEGIN
+                    DECLARE CONTINUE HANDLER FOR NOT FOUND
+                        SET new_driver_id = NULL;
+
+                    -- Query should be a unique or non-unique index lookup on idx_driver_unq
+                    SELECT id INTO new_driver_id
+                    FROM Driver
+                    WHERE ((driver_name IS NULL) OR driver_name = new_drv_name)
+                        AND driver_name = new_drv_name
+                        AND country = new_drv_cntry;
+
+                    IF new_driver_id IS NULL THEN
+                        INSERT INTO Driver (driver_name, country)
+                        VALUES (new_drv_name, new_drv_cntry);
+                        SET new_driver_id = LAST_INSERT_ID();
+                    END IF;
+                END;
             END IF;
 
-            SET new_drv_ord = 0;
+            -- Create car
+            IF res_car_chassis != '' OR res_car_engine != '' THEN
+                BEGIN
+                    DECLARE CONTINUE HANDLER FOR NOT FOUND
+                        SET new_car_id = NULL;
 
-            -- Start a transaction
-            START TRANSACTION;
-            WHILE res_drivers_name IS NOT NULL DO
-                SET new_drv_title = NULL;
-                SET new_drv_name = SUBSTRING_INDEX(res_drivers_name, '|', 1);
-                SET new_drv_nick = NULL;
-                SET new_drv_nm_sfx = NULL;
-                SET new_drv_cntry = SUBSTRING_INDEX(res_drivers_cntry, '|', 1);
-                SET new_drv_sex = 'M';
-                SET new_drv_ord = new_drv_ord + 1;
+                    SET res_car_class = NULLIF(res_car_class, '');
+                    SET res_car_engine = NULLIF(res_car_engine, '');
 
-                -- Detect females
-                IF new_drv_name LIKE 'Mme%' OR new_drv_name LIKE 'Miss%' OR new_drv_name LIKE 'Mrs.%' THEN
-                    SET new_drv_sex = 'F';
-                    SET new_drv_name = SUBSTR(new_drv_name, INSTR(new_drv_name, ' ') + 1);
-                END IF;
+                    -- Check if car exists
+                    SELECT id INTO new_car_id
+                    FROM Car
+                    WHERE car_chassis = res_car_chassis
+                        AND ((car_engine IS NULL AND res_car_engine IS NULL) OR car_engine = res_car_engine);
 
-                IF new_drv_name != '' AND new_drv_cntry != '' THEN
-                    SET tmp_drv_name = new_drv_name;
-
-                    IF new_drv_name LIKE '%''%''%' THEN
-                        -- name with two single quotes
-                        SET fst_qt_idx = INSTR(new_drv_name, '''');
-                        SET snd_qt_idx = LOCATE('''', new_drv_name, fst_qt_idx + 1);
-
-                        SET tmp_drv_name = SUBSTRING(new_drv_name, 1, fst_qt_idx - 1);
-                        SET tmp_drv_name = CONCAT(tmp_drv_name, TRIM(SUBSTRING(new_drv_name, snd_qt_idx + 1)));
-                        SET new_drv_nick = SUBSTRING(new_drv_name, fst_qt_idx + 1, snd_qt_idx - fst_qt_idx - 1);
+                    -- If it doesn't exist, then create it
+                    IF new_car_id IS NULL THEN
+                        INSERT INTO Car (car_class_id, car_chassis, car_engine )
+                        VALUES (res_car_class, res_car_chassis, res_car_engine);
+                        SET new_car_id = LAST_INSERT_ID();
                     END IF;
+                END;
+            ELSE
+                -- car not defined
+                SET new_car_id = NULL;
+            END IF;
 
-                    -- clean braces in name and trim
-                    SET tmp_drv_name = REPLACE(tmp_drv_name, '(', '');
-                    SET tmp_drv_name = REPLACE(tmp_drv_name, ')', '');
-                    SET new_drv_name = TRIM(tmp_drv_name);
+            INSERT IGNORE INTO CarNumber (race_id, car_number, car_id)
+            VALUES (res_race_yr, res_car_nbr, new_car_id);
 
-                    BEGIN
-                        DECLARE CONTINUE HANDLER FOR NOT FOUND
-                            SET new_driver_id = NULL;
+            SET new_carnb_id = LAST_INSERT_ID();
 
-                        -- Query should be a unique or non-unique index lookup on idx_driver_unq
-                        SELECT id INTO new_driver_id
-                        FROM Driver
-                        WHERE (
-                            (driver_name IS NULL)
-                            OR driver_name = new_drv_name
-                        )
-                            AND driver_name = new_drv_name
-                            AND country = new_drv_cntry;
+            IF res_car_tyres IS NOT NULL THEN
+                INSERT IGNORE INTO CarTyre (car_id, tyre_id)
+                SELECT new_carnb_id, id
+                    FROM Tyre
+                WHERE tyre_name = res_car_tyres;
+            END IF;
 
-                        IF new_driver_id IS NULL THEN
-                            INSERT INTO Driver (driver_name, country)
-                            VALUES (new_drv_name, new_drv_cntry);
-                            SET new_driver_id = LAST_INSERT_ID();
-                        END IF;
-                    END;
+            -- Create result 
+            INSERT IGNORE INTO Result (
+                race_id,
+                car_number,
+                pos,
+                laps,
+                distance,
+                racing_time,
+                retirement_reason
+            )
+            VALUES (
+                res_race_yr,
+                res_car_nbr,
+                res_pos,
+                res_laps,
+                res_distance,
+                res_racing_time,
+                res_reason
+            );
 
-                END IF;
-                /* Create car */
-    IF  res_car_chassis != '' OR res_car_engine != '' THEN
-      BEGIN
-        DECLARE CONTINUE HANDLER FOR NOT FOUND SET new_car_id = NULL;
-
-        SET res_car_class  = NULLIF(res_car_class, '');
-        SET res_car_engine = NULLIF(res_car_engine, '');
-
-        /* Check if car exists */
-        SELECT id
-          INTO new_car_id
-          FROM Car
-         WHERE  car_chassis = res_car_chassis
-           AND (   (car_engine IS NULL AND res_car_engine IS NULL)
-                OR car_engine  = res_car_engine
-               );
-
-        /* else create it */
-        IF new_car_id IS NULL THEN
-          INSERT INTO Car (car_chassis, car_engine)
-          VALUES ( res_car_chassis, res_car_engine);
-          SET new_car_id = LAST_INSERT_ID();
-        END IF;
-      END;
-    ELSE /* car not defined */
-      SET new_car_id = NULL;
+    /* Strip double quotes */
+    IF res_team_name LIKE '%"' THEN
+      SET res_team_name = REPLACE(SUBSTR(res_team_name, 1, CHAR_LENGTH(res_team_name) - 1), '""', '"');
     END IF;
 
-    INSERT IGNORE INTO CarNumber (race_id, car_number, car_id)
-    VALUES (res_race_yr, res_car_nbr, new_car_id);
+    /* Process team(s) */
+    SET new_team_ord = 0;
+    WHILE res_team_name IS NOT NULL DO
+      SET new_team_name  = SUBSTRING_INDEX(res_team_name, '|', 1);
+      SET new_team_cntry = SUBSTRING_INDEX(res_team_cntry, '|', 1);
+      SET new_team_priv = FALSE;
+      SET new_team_ord   = new_team_ord + 1;
 
-    SET new_carnb_id = LAST_INSERT_ID();
-                 /* Create result */
-            INSERT IGNORE INTO Result (race_id, car_number, pos, laps, distance, racing_time, retirement_reason)
-            VALUES (res_race_yr, res_car_nbr, res_pos, res_laps, res_distance, res_racing_time, res_reason);
+      /* Detect private entrants */
+      IF new_team_name LIKE '% (private entrant)' THEN
+        SET new_team_priv = TRUE;
+        SET new_team_name = REPLACE(new_team_name, ' (private entrant)', '');
+      END IF;
 
-                SET new_res_id = LAST_INSERT_ID();
-                IF new_driver_id IS NOT NULL AND new_res_id IS NOT NULL THEN
-                    INSERT IGNORE INTO DriverResult (driver_id, race_id, car_number, driver_order)
-                    VALUES (new_driver_id, res_race_yr, res_car_nbr, new_drv_ord);
-                END IF;
+      IF new_team_name != '' THEN
+        BEGIN
+          DECLARE CONTINUE HANDLER FOR NOT FOUND SET new_team_id = NULL;
 
-                IF NOT INSTR(res_drivers_name, '|') THEN
-                    SET res_drivers_name = NULL;
-                    SET res_drivers_cntry = NULL;
-                ELSE
-                    SET res_drivers_name = SUBSTR(res_drivers_name, INSTR(res_drivers_name, '|') + 1);
-                    SET res_drivers_cntry = SUBSTR(res_drivers_cntry, INSTR(res_drivers_cntry, '|') + 1);
-                END IF;
+          SET new_team_cntry = NULLIF(new_team_cntry, '');
 
-            END WHILE;
+          SELECT id
+            INTO new_team_id
+            FROM Team
+           WHERE team_name   = new_team_name
+             AND (   (country IS NULL AND new_team_cntry IS NULL)
+                  OR country = new_team_cntry
+                 );
 
-            -- Commit the transaction
-            COMMIT;
-        END LOOP;
-    END;
+          IF new_team_id IS NULL THEN
+            INSERT INTO Team (team_name, country)
+            VALUES (new_team_name, new_team_cntry);
 
-END //
+            SET new_team_id = LAST_INSERT_ID();
+          END IF;
+        END;
 
+            INSERT IGNORE INTO TeamResult (team_id, race_id, car_number, ord_num)
+        VALUES (new_team_id, res_race_yr, res_car_nbr, new_team_ord);
+      END IF;
+
+      IF NOT INSTR(res_team_name, '|') THEN
+        SET res_team_name  = NULL;
+        SET res_team_cntry = NULL;
+      ELSE
+        SET res_team_name  = SUBSTR(res_team_name , INSTR(res_team_name , '|') + 1);
+        SET res_team_cntry = SUBSTR(res_team_cntry, INSTR(res_team_cntry, '|') + 1);
+      END IF;
+    END WHILE; /* teams */
+
+            SET new_res_id = LAST_INSERT_ID();
+
+            IF new_driver_id IS NOT NULL AND new_res_id IS NOT NULL THEN
+                INSERT IGNORE INTO DriverResult (driver_id, race_id, car_number, driver_order, team_name)
+                VALUES (
+                    new_driver_id,
+                    res_race_yr,
+                    res_car_nbr,
+                    new_drv_ord,
+                    new_team_name
+                );
+            END IF;
+
+            IF NOT INSTR(res_drivers_name, '|') THEN
+                SET res_drivers_name = NULL;
+                SET res_drivers_cntry = NULL;
+            ELSE
+                SET res_drivers_name = SUBSTR(res_drivers_name, INSTR(res_drivers_name, '|') + 1);
+                SET res_drivers_cntry = SUBSTR(res_drivers_cntry, INSTR(res_drivers_cntry, '|') + 1);
+            END IF;
+        END WHILE;
+
+        -- Commit the transaction
+        COMMIT;
+    END LOOP;
+END;
+END// 
 DELIMITER ;
-
 
 call process_drivers();
 
-select * from Result limit 10;
+select *
+from Result
+limit 10;
